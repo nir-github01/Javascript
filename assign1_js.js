ef41c00bd88915f517359ba128eeecaa1bfa3824
Niradity Emmanuel <yadavrjking66997@gmail.com>
2022-10-15 23:57:34 +0530

    Date of Birth and Age Calculation

---
+++ assign1_js.js
@@ -0,0 +1,33 @@
+
+function getDob()
+{
+  
+    const age_val =document.getElementById('age').value;
+  
+    const dob_Val=document.getElementById('dob').value;
+    const dob_date =new Date(dob_Val);
+    const dob_day=dob_date.getDate();
+    const dob_month =dob_date.getMonth();
+    const dob_year =dob_date.getFullYear();
+    const date=new Date;
+    const curr_day=date.getDate();
+    const curr_month=date.getMonth() + 1;
+    const curr_year =date.getFullYear();
+ 
+    // const month_diff= Date.now() - dob_Val.getTime();
+    // alert(month_diff);
+   // alert(year )
+    document.getElementById('cdate').innerHTML="Today is" + date;
+    document.getElementById('ageval').innerHTML="Your age is" + age_val;
+ 
+ 
+    if(dob_Val != '' ){
+       
+       let dob_val =prompt("Your Date of Birth is-", dob_Val);
+       let curr_val =prompt("Today Date is-", `${curr_day}-${curr_month}-${curr_year}`);
+       document.getElementById("output").innerHTML= "Your date of Birth is"+ "<br>" + dob_val + "\n"+ "Today is"+ curr_val;
+        alert(dob_Val);
+    }
+ 
+    //document
+}
